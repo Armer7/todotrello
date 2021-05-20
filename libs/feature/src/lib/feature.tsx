c54c188e9112b5ba997cbf0faeeb8750afae374a
Aleksey Poslushnyak <Poslushnyak@2amigos.us>
2021-05-20 19:33:37 +0300

    first commit

---
+++ libs/feature/src/lib/feature.tsx
@@ -0,0 +1,28 @@
+
+import React from 'react';
+
+
+
+
+
+  import './feature.module.scss';
+  
+
+/* eslint-disable-next-line */
+export interface FeatureProps {
+}
+
+
+
+export function Feature(props: FeatureProps) {
+  return (
+    <div>
+      
+      <h1>Welcome to feature!</h1>
+      
+    </div>
+  );
+};
+
+
+export default Feature;
