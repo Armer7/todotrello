4bcde40b3c4c0a6594d6e3bf1e2469731e71ab33
Aleksey Poslushnyak <Poslushnyak@2amigos.us>
2021-06-02 09:53:37 +0300

    add import module datePickers

--- apps/dashboard/src/app/app.tsx
+++ apps/dashboard/src/app/app.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { MuiPickersUtilsProvider } from '@material-ui/pickers';
+import DateFnsUtils from '@date-io/date-fns';
+
 import TodosPage from './pages/TodosPage';
 import CreateCard from './pages/CreateCard';
 import EditCard from './pages/EditCard';
 
 const App: React.FC = () => {
   return (
-    <Switch>
-      <Route exact path="/" component={TodosPage} />
-      <Route path="/create-card" component={CreateCard} />
-      <Route path="/edit-card/:id" component={EditCard} />
-    </Switch>
+    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+      <Switch>
+        <Route exact path="/" component={TodosPage} />
+        <Route path="/create-card" component={CreateCard} />
+        <Route path="/edit-card/:id" component={EditCard} />
+      </Switch>
+    </MuiPickersUtilsProvider>
   );
 };
 
