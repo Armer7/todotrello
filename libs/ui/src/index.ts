0be065b8b53745c0262e5e9c1e3a3bd559bd0dd6
Aleksey Poslushnyak <Poslushnyak@2amigos.us>
2021-06-02 09:52:20 +0300

    add component library

--- libs/ui/src/index.ts
+++ libs/ui/src/index.ts
@@ -1,3 +1,9 @@
+
+export * from './lib/check-list/CheckList';
+export * from './lib/check-box-state-task/CheckBoxStateTask';
+export * from './lib/top-text-of-block/TopTextOfBlock';
+export * from './lib/list-labels/ListLabels';
+export * from './lib/select-box/SelectBox';
 export * from './lib/formCard/FormCard';
 export * from './lib/card/card';
 export * from './lib/Ui';
